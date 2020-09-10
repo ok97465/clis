aee36a2bcc3ddf5f6bcded6d5552e603ab9e310b
ok97465 <ok97465@gmail.com>
2020-09-11 01:03:12 +0900

    Initial version

---
+++ archive.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+import os.path as osp
+from zipfile import ZipFile, ZIP_DEFLATED
+
+if len(sys.argv) < 2:
+    sys.exit()
+
+if len(sys.argv) == 2:
+    root_folder, filename = osp.split(sys.argv[1])
+    name = osp.splitext(filename)[0]
+else:
+    root_folder, _ = osp.split(sys.argv[1])
+    name = osp.split(root_folder)[1]
+
+path = osp.join(root_folder, name + '.zip')
+idx_name = -1
+while osp.isfile(path):
+    path = osp.join(root_folder, name + f'{idx_name}.zip')
+
+with ZipFile(path, 'w', ZIP_DEFLATED) as zf:
+    for arg in sys.argv[1:]:
+        zf.write(arg, osp.basename(arg))
+        if not osp.isdir(arg):
+            continue
+        path_subdir = Path(arg).expanduser().resolve(strict=True)
+
+        for path_file in path_subdir.glob('**/*'):
+            if '__pycache__' in path_file.parts:
+                continue
+            zf.write(path_file, path_file.relative_to(root_folder))
+
---
+++ extract.py
@@ -0,0 +1,75 @@
+"""."""
+# Standard library imports
+import os
+import os.path as osp
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+from zipfile import ZipFile
+
+
+def get_outfolder_tmp(path: str):
+    """압축을 풀 임시 폴더 이름을 생성한다."""
+    folder_archive = osp.dirname(path)
+    folder_tmp = osp.join(folder_archive, '_tmp_ok97465_')
+    return folder_tmp
+
+
+def move_output(path):
+    """임시 폴더에 압축을 푼 후 임시 폴더의 파일을 유효한 폴더로 이동한다."""
+    folder_base = osp.dirname(path)
+    folder_tmp = get_outfolder_tmp(path)
+
+    list_out = os.listdir(folder_tmp)
+
+    if len(list_out) == 1:
+        src = osp.join(folder_tmp, list_out[0])
+        shutil.move(src, folder_base)
+        shutil.rmtree(folder_tmp)
+        return
+
+    basename = osp.splitext(osp.basename(path))[0]
+    folder_out = osp.join(folder_base, basename)
+
+    idx = -1
+    while(osp.isdir(folder_out)):
+        idx += 1
+        folder_out = osp.join(folder_base, f'{basename}_{idx}_')
+
+    shutil.move(folder_tmp, folder_out)
+
+
+def extract_zip(path):
+    """Zip 확장자 파일의 압축을 푼다."""
+    folder_tmp = get_outfolder_tmp(path)
+    with ZipFile(path, 'r') as zf:
+        zf.extractall(folder_tmp)
+
+    move_output(path)
+
+
+def extract_iso(path):
+    """ISO 확장자 파일의 압축을 푼다."""
+    folder_tmp = get_outfolder_tmp(path)
+    proc = subprocess.Popen(['7z', 'x', path, f'-o{folder_tmp}'],
+                            stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+    print(out.decode('utf-8'))
+    move_output(path)
+
+
+if len(sys.argv) < 2:
+    sys.exit()
+
+for arg in sys.argv[1:]:
+    path = Path(arg)
+    path = path.expanduser().resolve(strict=True)
+    ext = path.suffix.lower()
+    try:
+        if ext == '.zip':
+            extract_zip(path)
+        elif ext == '.iso':
+            extract_iso(path)
+    except Exception as e:
+        print(e)
